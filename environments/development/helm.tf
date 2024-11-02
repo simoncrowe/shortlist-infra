ea96243c2661b3371fdd0147c1fe598bac1210b0
Simon Crowe <simon.r.crowe@protonmail.com>
2024-11-02 15:38:37 +0000

    Start on ingester chart and redis

--- environments/development/helm.tf
+++ environments/development/helm.tf
@@ -1,6 +1,5 @@
 data "google_client_config" "provider" {}
 
-
 provider "helm" {
   kubernetes {
     host  = "https://${google_container_cluster.primary.endpoint}"
@@ -15,7 +14,7 @@ provider "helm" {
   }
 }
 
-resource "helm_release" "shortlist_runner" {
+resource "helm_release" "runner" {
   name       = "dev"
   repository = "https://simoncrowe.github.io/helm"
   chart      = "shortlist-runner"
@@ -34,4 +33,54 @@ resource "helm_release" "shortlist_runner" {
     value = "http://dev-shortlist-notifier.shortlist.svc.cluster.local/api/v1/notifcations"
   }
 
+}
+
+resource "helm_release" "rm_ingester" {
+  name = "dev"
+  repository = "https://simoncrowe.github.io/helm"
+  chart      = "shortlist-rm-ingester"
+
+  namespace        = "shortlist"
+  create_namespace = true
+  
+  set {
+    name = "redis.hostname"
+    value = "${helm_release.redis.name}-master.shortlist.svc.cluster.local"
+  }
+  set {
+    name = "redis.password"
+    value = random_password.redis_password.result  
+  }
+  set {
+    name = "ingester.runnerUrl" 
+    value = "http://runner.shortlist.svc.cluster.local/api/v1/profiles"
+  }
+  set {
+    name = "ingester.resultsUrl"
+    value = var.rm_results_url
+  }
+
+}
+
+resource "random_password" "redis_password" {
+  length = 32
+  special = true
+}
+
+resouuce "helm_release" "redis" {
+  name = "redis-dev"
+  repository  = "oci://registry-1.docker.io/bitnamicharts"
+  chart = "redis"
+
+  namespace = "shortlist"
+  create_namespace = "true"
+
+  set {
+    name = "architecture"
+    value = "standalone"
+  }
+  set {
+    name = "auth.password"
+    value = random_password.redis_password.result
+  }
 }
